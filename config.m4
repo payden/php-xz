0e94c7a5ea590ef4f50d243de86549ba29b1bd22
Payden Sutherland <payden@paydensutherland.com>
2013-02-24 15:17:00 -0500

    Initial commit of xz PHP extension

---
+++ config.m4
@@ -0,0 +1,61 @@
+dnl $Id$
+dnl config.m4 for extension xz
+
+dnl Comments in this file start with the string 'dnl'.
+dnl Remove where necessary. This file will not work
+dnl without editing.
+
+dnl If your extension references something external, use with:
+
+PHP_ARG_WITH(xz, for xz support,
+[  --with-xz             Include xz support])
+
+dnl Otherwise use enable:
+
+dnl PHP_ARG_ENABLE(xz, whether to enable xz support,
+dnl Make sure that the comment is aligned:
+dnl [  --enable-xz           Enable xz support])
+
+if test "$PHP_XZ" != "no"; then
+  dnl Write more examples of tests here...
+
+  dnl # --with-xz -> check with-path
+  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
+  dnl SEARCH_FOR="/include/xz.h"  # you most likely want to change this
+  dnl if test -r $PHP_XZ/$SEARCH_FOR; then # path given as parameter
+  dnl   XZ_DIR=$PHP_XZ
+  dnl else # search default path list
+  dnl   AC_MSG_CHECKING([for xz files in default path])
+  dnl   for i in $SEARCH_PATH ; do
+  dnl     if test -r $i/$SEARCH_FOR; then
+  dnl       XZ_DIR=$i
+  dnl       AC_MSG_RESULT(found in $i)
+  dnl     fi
+  dnl   done
+  dnl fi
+  dnl
+  dnl if test -z "$XZ_DIR"; then
+  dnl   AC_MSG_RESULT([not found])
+  dnl   AC_MSG_ERROR([Please reinstall the xz distribution])
+  dnl fi
+
+  dnl # --with-xz -> add include path
+  dnl PHP_ADD_INCLUDE($XZ_DIR/include)
+
+  dnl # --with-xz -> check for lib and symbol presence
+  LIBNAME=lzma # you may want to change this
+  LIBSYMBOL=lzma_stream_encoder # you most likely want to change this 
+
+  PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
+  [
+    PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $XZ_DIR/lib, XZ_SHARED_LIBADD)
+    AC_DEFINE(HAVE_XZLIB,1,[ ])
+  ],[
+    AC_MSG_ERROR([wrong xz lib version or lib not found])
+  ],[
+    -L$XZ_DIR/lib -lm
+  ])
+  PHP_SUBST(XZ_SHARED_LIBADD)
+
+  PHP_NEW_EXTENSION(xz, xz.c xz_fopen_wrapper.c, $ext_shared)
+fi
---
+++ php_xz.h
@@ -0,0 +1,85 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2013 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Payden Sutherland <payden@paydensutherland.com>              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#ifndef PHP_XZ_H
+#define PHP_XZ_H
+
+extern zend_module_entry xz_module_entry;
+#define phpext_xz_ptr &xz_module_entry
+
+#define XZ_INBUF_SIZE 4096
+#define XZ_OUTBUF_SIZE 4096
+
+#ifdef PHP_WIN32
+#	define PHP_XZ_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_XZ_API __attribute__ ((visibility("default")))
+#else
+#	define PHP_XZ_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(xz);
+PHP_MSHUTDOWN_FUNCTION(xz);
+PHP_RINIT_FUNCTION(xz);
+PHP_RSHUTDOWN_FUNCTION(xz);
+PHP_MINFO_FUNCTION(xz);
+
+PHP_FUNCTION(xzopen);
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+
+ZEND_BEGIN_MODULE_GLOBALS(xz)
+	long  global_value;
+	char *global_string;
+ZEND_END_MODULE_GLOBALS(xz)
+*/
+
+/* In every utility function you add that needs to use variables 
+   in php_xz_globals, call TSRMLS_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMLS_CC
+   after the last function argument and declare your utility function
+   with TSRMLS_DC after the last declared argument.  Always refer to
+   the globals in your function as XZ_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define XZ_G(v) TSRMG(xz_globals_id, zend_xz_globals *, v)
+#else
+#define XZ_G(v) (xz_globals.v)
+#endif
+
+#endif	/* PHP_XZ_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
